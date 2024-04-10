e889c8ec3512092f49f6bfbf1cfb89fe3f22b52c
Liam Meredith <liam.meredith.clarke@gmail.com>
2024-04-10 20:43:56 +0200

    cards & attempts design

--- src/app/view-record/view-record.component.ts
+++ src/app/view-record/view-record.component.ts
@@ -16,4 +16,12 @@ export class ViewRecordComponent {
     [OptionValues.UP]: 'fa fa-arrow-up',
     [OptionValues.DOWN]: 'fa fa-arrow-down',
   };
+
+  public optionValueBg: Record<string, string> = {
+    [OptionValues.GREEN]: 'bg-success',
+    [OptionValues.RED]: 'bg-danger',
+    [OptionValues.YELLOW]: 'bg-warning',
+    [OptionValues.UP]: 'bg-info',
+    [OptionValues.DOWN]: 'bg-info',
+  };
 }
